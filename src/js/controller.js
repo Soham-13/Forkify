16a267ea123499f5ab8c07fcc6255e1b9a25e968
Soham-13 <manglamsoham5140@gmail.com>
2023-06-19 18:20:25 +0530

    cleaned up the controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,9 +6,6 @@ import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
-
-// import 'core-js/stable';
-// import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
 const controlRecipes = async function () {
