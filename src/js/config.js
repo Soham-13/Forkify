25c3b593b01d91a4052a0d117f12f9f18e0c92d7
Soham-13 <manglamsoham5140@gmail.com>
2023-06-19 18:13:33 +0530

    initail commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6228aed1-05c9-4f0c-97d2-c15449fa22a4';
+export const MODAL_CLOSE_SEC = 2.5;
